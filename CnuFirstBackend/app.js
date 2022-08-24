cc6152b45e58c792a78ad9ad8b1f46312a76b187
GreenBNN <kgh980316@gmail.com>
2022-08-24 12:01:36 +0900

    add Cnu

---
+++ CnuFirstBackend/app.js
@@ -0,0 +1,10 @@
+// 웹 서버
+const express = require('express')
+const app = express();
+
+// server 라는 변수에 저장
+const server = app.listen(8000, a)
+
+function a() {
+    console.log('server has started')
+}
